461078d60bec683aa8ea8776da7d3d1f55263a42
Dimitar Dimitrov <wireman@gmail.com>
2011-09-02 21:09:22 +0300

    Add message key processing in QueuedMail.

--- lib/smailer/models/queued_mail.rb
+++ lib/smailer/models/queued_mail.rb
@@ -1,3 +1,5 @@
+require 'digest/md5'
+
 module Smailer
   module Models
     class QueuedMail < ActiveRecord::Base
@@ -5,12 +7,16 @@ module Smailer
 
       validates_presence_of :mail_campaign_id, :to
       validates_uniqueness_of :to, :scope => :mail_campaign_id
+      validates_uniqueness_of :key
       validates_numericality_of :mail_campaign_id, :retries, :only_integer => true, :allow_nil => true
       validates_length_of :to, :last_error, :maximum => 255
 
       attr_accessible :mail_campaign_id, :to
 
       delegate :from, :subject, :mailing_list, :to => :mail_campaign, :allow_nil => true
+
+      before_validation :initialize_message_key
+      before_save :initialize_message_key
 
       def body_html
         interpolate mail_campaign.body_html
@@ -20,7 +26,16 @@ module Smailer
         interpolate mail_campaign.body_text
       end
 
+      def key
+        initialize_message_key
+        self[:key]
+      end
+
       protected
+
+      def initialize_message_key
+        self.key = Digest::MD5.hexdigest("#{mail_campaign_id}, #{to} and #{id} compose this key.")
+      end
 
       def interpolate(text)
         {
@@ -29,6 +44,7 @@ module Smailer
           :email_key        => lambda { MailKey.get(to) },
           :mailing_list_id  => lambda { mailing_list.id },
           :mail_campaign_id => mail_campaign_id,
+          :message_key      => key,
         }.each do |variable, interpolation|
           text.gsub! "%{#{variable}}" do
             interpolation.respond_to?(:call) ? interpolation.call : interpolation.to_s
