e47738ad45c7b53c04ca4efec681a547d181b44a
Dimitar Dimitrov <wireman@gmail.com>
2011-09-02 19:03:12 +0300

    Indentation fixes in models/property.

--- lib/smailer/models/property.rb
+++ lib/smailer/models/property.rb
@@ -1,52 +1,53 @@
 module Smailer
   module Models
     class Property < ActiveRecord::Base
-        validates_presence_of :name
-        validates_uniqueness_of :name
 
-        attr_accessible :name, :value, :notes
+      validates_presence_of :name
+      validates_uniqueness_of :name
 
-        @@cache_created_at = Time.now
+      attr_accessible :name, :value, :notes
 
-        def self.clear_cache_if_needed!
-          if Time.now - @@cache_created_at > 1.minute
-            remove_class_variable :@@properties rescue nil
-            @@cache_created_at = Time.now
-          end
+      @@cache_created_at = Time.now
+
+      def self.clear_cache_if_needed!
+        if Time.now - @@cache_created_at > 1.minute
+          remove_class_variable :@@properties rescue nil
+          @@cache_created_at = Time.now
+        end
+      end
+
+      def self.setup_cache!
+        self.clear_cache_if_needed!
+        @@properties ||= {} unless defined?(@@properties)
+      end
+
+      def self.get(name)
+        self.setup_cache!
+        name = name.to_s
+
+        unless @@properties.has_key?(name)
+          property = find_by_name name
+          @@properties[name] = property ? property.value : nil
         end
 
-        def self.setup_cache!
-          self.clear_cache_if_needed!
-          @@properties ||= {} unless defined?(@@properties)
+        @@properties[name]
+      end
+
+      def self.get_all(name)
+        self.setup_cache!
+        name, cache_key = name.to_s, "#{name}[]"
+
+        unless @@properties.has_key?(cache_key)
+          properties = all(:conditions => ['name LIKE ?', "#{name}%"]).map { |p| [p.name, p.value] }
+          @@properties[cache_key] = Hash[ properties ]
         end
 
-        def self.get(name)
-          self.setup_cache!
-          name = name.to_s
+        @@properties[cache_key]
+      end
 
-          unless @@properties.has_key?(name)
-            property = find_by_name name
-            @@properties[name] = property ? property.value : nil
-          end
-
-          @@properties[name]
-        end
-
-        def self.get_all(name)
-          self.setup_cache!
-          name, cache_key = name.to_s, "#{name}[]"
-
-          unless @@properties.has_key?(cache_key)
-            properties = all(:conditions => ['name LIKE ?', "#{name}%"]).map { |p| [p.name, p.value] }
-            @@properties[cache_key] = Hash[ properties ]
-          end
-
-          @@properties[cache_key]
-        end
-
-        def self.get_boolean(name)
-          self.get(name).to_s.strip =~ /^(true|t|yes|y|on|1)$/i ? true : false
-        end
+      def self.get_boolean(name)
+        self.get(name).to_s.strip =~ /^(true|t|yes|y|on|1)$/i ? true : false
+      end
     end
   end
 end
