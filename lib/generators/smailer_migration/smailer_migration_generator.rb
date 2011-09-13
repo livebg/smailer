a5199284d9a685097d4baeb29dc7314fa20e7220
Dimitar Dimitrov <wireman@gmail.com>
2011-09-13 12:46:11 +0300

    First attempt at making a migration generator.

---
+++ lib/generators/smailer_migration/smailer_migration_generator.rb
@@ -0,0 +1,17 @@
+class SmailerMigrationGenerator < Rails::Generators::Base
+  desc "This generator creates a migration file containing definitions of the tables needed to run Smailer."
+
+  def create_migration_file
+    root        = File.dirname(__FILE__)
+    source      = File.expand_path 'templates/migration.rb', root
+    destination = Rails.root.join("db/migrate/#{next_migration_number}_create_smailer_tables.rb")
+
+    FileUtils.cp source, destination
+  end
+
+  protected
+
+  def next_migration_number
+    Time.now.utc.strftime("%Y%m%d%H%M%S")
+  end
+end
