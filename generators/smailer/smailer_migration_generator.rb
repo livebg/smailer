0d7623825190e55b7d944d4850436edfe32de69f
Dimitar Dimitrov <wireman@gmail.com>
2011-09-13 19:52:51 +0300

    Version 0.2.8 - Rails 2 migration generator template paths fix.

--- generators/smailer/smailer_migration_generator.rb
+++ generators/smailer/smailer_migration_generator.rb
@@ -2,10 +2,9 @@ class SmailerMigrationGenerator < Rails::Generator::Base
   def manifest
     file_name       = 'create_smailer_tables'
     @migration_name = file_name.camelize
-    template_path   = File.expand_path('../../lib/generators/smailer/templates/migration.rb.erb', File.dirname(__FILE__))
 
     record do |m|
-      m.migration_template template_path, File.join('db', 'migrate'), :migration_file_name => file_name
+      m.migration_template 'migration.rb.erb', File.join('db', 'migrate'), :migration_file_name => file_name
     end
   end
 end
--- lib/smailer/version.rb
+++ lib/smailer/version.rb
@@ -1,3 +1,3 @@
 module Smailer
-  VERSION = "0.2.7"
+  VERSION = "0.2.8"
 end
