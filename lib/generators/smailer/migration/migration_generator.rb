9d9a956bd2d01c3ed232e3181dddfac50d02f329
Dimitar Dimitrov <wireman@gmail.com>
2011-09-13 18:48:12 +0300

    More fixes for the Rails 3 migration generator.

---
+++ lib/generators/smailer/migration/migration_generator.rb
@@ -0,0 +1,16 @@
+module Smailer
+  class MigrationGenerator < Rails::Generators::Base
+    include Rails::Generators::Migration
+    extend ActiveRecord::Generators::Migration
+
+    desc "Create a migration file with definitions of the tables needed to run Smailer."
+    source_root File.expand_path('../templates', __FILE__)
+
+    def generate_migration
+      file_name       = 'create_smailer_tables'
+      @migration_name = file_name.camelize
+
+      migration_template 'migration.rb.erb', "db/migrate/#{file_name}.rb"
+    end
+  end
+end
