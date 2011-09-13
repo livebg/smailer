d09091b736b9bc24de44358f1c03414b152c6b46
Dimitar Dimitrov <wireman@gmail.com>
2011-09-13 20:37:14 +0300

    Version 0.2.10 - fix generated migration in Rails 2 projects.
    I'm just gonna flip out anytime now.

--- lib/generators/smailer/migration_generator.rb
+++ lib/generators/smailer/migration_generator.rb
@@ -10,7 +10,6 @@ module Smailer
 
     def generate_migration
       file_name       = 'create_smailer_tables'
-      @migration_name = file_name.camelize
 
       migration_template 'migration.rb.erb', "db/migrate/#{file_name}.rb"
     end
