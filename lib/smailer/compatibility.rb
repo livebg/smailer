4d4ceddea9151c3e922ab6944a71700b1c09ab6b
Dimitar Dimitrov <wireman@gmail.com>
2011-09-16 17:26:02 +0300

    Add missing receiver in Compatibility.save_without_validation.

--- lib/smailer/compatibility.rb
+++ lib/smailer/compatibility.rb
@@ -5,7 +5,7 @@ module Smailer
     end
 
     def self.save_without_validation(object)
-      rails_3? ? object.save(:validate => false) : save(false)
+      rails_3? ? object.save(:validate => false) : object.save(false)
     end
   end
 end
