ea9f4377c8bb9dea4e36ab669f2648c7743c4798
Dimitar Dimitrov <me@ddimitrov.name>
2014-12-01 16:37:18 +0200

    Use extend self for Smailer::Compatibility
    This is a simpler and shorter way to create a module with only module
    methods.

--- lib/smailer/compatibility.rb
+++ lib/smailer/compatibility.rb
@@ -1,18 +1,20 @@
 module Smailer
   module Compatibility
-    def self.rails_3?
+    extend self
+
+    def rails_3?
       Rails::VERSION::MAJOR == 3
     end
 
-    def self.rails_4?
+    def rails_4?
       Rails::VERSION::MAJOR == 4
     end
 
-    def self.rails_3_or_4?
-      self.rails_3? || self.rails_4?
+    def rails_3_or_4?
+      rails_3? || rails_4?
     end
 
-    def self.save_without_validation(object)
+    def save_without_validation(object)
       rails_3_or_4? ? object.save(:validate => false) : object.save(false)
     end
   end
