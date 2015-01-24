aed86c615b7494c10962e2fe8aa255d4f16de0b5
noijd <ju.an.marluc@gmail.com>
2015-01-24 22:01:48 +0000

    Create the RSpec configuration with a dummy app for testing

---
+++ spec/models/mail_key_spec.rb
@@ -0,0 +1,14 @@
+require 'spec_helper'
+
+describe Smailer::Models::MailKey do
+
+  describe 'associations' do
+  end
+
+  describe 'indexes' do
+  end
+
+  describe 'validations' do
+  end
+  
+end
